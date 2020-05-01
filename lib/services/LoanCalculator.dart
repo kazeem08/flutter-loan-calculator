707215a189663226f06261983ac7af5446560778
kazeem <kazeem0825@gmail.com>
2020-05-01 21:03:30 +0100

    add methods to get number of payments and interest per month

--- lib/services/LoanCalculator.dart
+++ lib/services/LoanCalculator.dart
@@ -1,10 +1,18 @@
 class LoanCalculator {
-  static int monthsInYear;
-  static int percent;
+  static int numberOfMonths = 12;
+  static int percent = 100;
 
   int _principal;
   double _annualInterest;
   int _years;
 
   LoanCalculator(this._principal, this._annualInterest, this._years);
+
+  int getNumberOfPayments() {
+    return _years * numberOfMonths;
+  }
+
+  double getInterestPerMonth() {
+    return (_annualInterest / percent / numberOfMonths);
+  }
 }
